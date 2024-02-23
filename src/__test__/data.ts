da4f436067fe719ad15e30271156362a3e0b60f1
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-02-23 04:11:27 +0100

    feat: Add unit-tests for exchange rate conversion

---
+++ src/__test__/data.ts
@@ -0,0 +1,8 @@
+// values are: how many of the currency is 1 USD
+export const TEST_RATES = {
+  SEK: 10,
+  USD: 1,
+  EUR: 0.9,
+  GBP: 0.75,
+  CNY: 5,
+};
---
+++ src/components/CurrencyRates.test.tsx
@@ -0,0 +1,30 @@
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+import { CurrencyRates } from "./CurrencyRates";
+import { TEST_RATES } from "../__test__/data";
+import "@testing-library/jest-dom";
+
+describe("CurrencyRates", () => {
+  it("renders without crashing", () => {
+    render(<CurrencyRates currency="SEK" rates={TEST_RATES} />);
+  });
+
+  it("renders currency rates correctly", async () => {
+    render(<CurrencyRates currency="SEK" rates={TEST_RATES} />);
+
+    const currencyElements = document.querySelectorAll(".rates li");
+
+    expect(currencyElements[0].textContent).toBe("1 USD = 10 SEK");
+    expect(currencyElements[1].textContent).toBe("1 EUR = 11.11 SEK");
+    expect(currencyElements[2].textContent).toBe("1 GBP = 13.33 SEK");
+    expect(currencyElements[3].textContent).toBe("1 CNY = 2 SEK");
+  });
+
+  it("does not show rates for target currency", () => {
+    const { queryByText } = render(
+      <CurrencyRates currency="SEK" rates={TEST_RATES} />
+    );
+
+    expect(queryByText("1 SEK = 1 SEK")).not.toBeInTheDocument();
+  });
+});
